5f37f9920038403179a5dbf9d3d7d3a87b4d4337
alex-pobeditel-2004 <shitfox@yandex.ru>
2017-08-29 23:13:22 +0300

    blackletter added

--- nib4pimp.py
+++ nib4pimp.py
@@ -19,17 +19,33 @@ def main():
         formatter_class=argparse.RawTextHelpFormatter
     )
     # debug: remove default, add required later
-    parser.add_argument("-f", "--font", type=str, metavar="FONT", choices=["trojan", "antiqua"], default="trojan",
-        help="Font which you want generate a grid for.\n" +
-             "Accepted values: trojan, antiqua")
-    parser.add_argument("-n", "--nib-size", type=float, metavar="NUMBER", required=True, help="Width of nib in millimeters (0.2...30)")
+    parser.add_argument("-f", "--font", type=str, metavar="FONT", choices=["1", "2", "3", "4", "5", "6", "7", "8", "9"], default="1",
+        help="Font which you want generate a grid for. Accepted values:\n" +
+             "1 - Roman square capitals\n" +
+             "2 - Antiqua Sans\n" +
+             "3 - Blackletter\n" +
+             "4 - Italic\n" +
+             "5 - Copperplate\n" +
+             "6 - Rustic\n" +
+             "7 - Insular script, Uncial, Ustav\n" +
+             "8 - Half-ustav\n" +
+             "9 - Caroline minuscule")
+    parser.add_argument("-n", "--nib-size", type=float, metavar="NUMBER", required=True,
+        help="Width of nib in millimeters. Accepted values:\n" +
+             "0.2 ... 30")
     parser.add_argument("-o", "--output-file", type=str, metavar="FILEPATH", required=True)
     parser.add_argument("-t", "--type", type=str, metavar="FILETYPE", choices=["PDF", "PNG", "SVG"], default="PDF",
-        help="Output filetype.\n" + 
-             "Accepted values: PDF, PNG, SVG")
-    parser.add_argument("-x", "--x-paper", type=float, metavar="NUMBER", default=210, help="Paper width in millimeters (50...5000)")
-    parser.add_argument("-y", "--y-paper", type=float, metavar="NUMBER", default=297, help="Paper heigth in millimeters (50...5000)")
-    parser.add_argument("-r", "--resolution", type=int, metavar="NUMBER", default=300, help="DPI value for PNG/SVG images (1...2400)")
+        help="Output filetype. Accepted values:\n" +
+             "PDF (default), PNG, SVG")
+    parser.add_argument("-x", "--x-paper", type=float, metavar="NUMBER", default=210,
+        help="Paper width in millimeters. Accepted values:\n" +
+             "50 ... 5000, default: 210")
+    parser.add_argument("-y", "--y-paper", type=float, metavar="NUMBER", default=297,
+        help="Paper heigth in millimeters. Accepted values:\n" +
+             "50 ... 5000, default: 297")
+    parser.add_argument("-r", "--resolution", type=int, metavar="NUMBER", default=300,
+        help="DPI value for PNG/SVG images Accepted values:\n" +
+             "1 ... 2400, default: 300")
     args = parser.parse_args()
 
 # Argument checks:
@@ -96,55 +112,61 @@ def create_surface(paper_size, filetype, output):
 
 def draw_grid(surface, context, font, nib_size, field):
     '''Choose proper draw function based on chosen font'''
-    if font == "trojan":
-        surface, context = trojan(surface, context, nib_size, field)
-    elif font == "antiqua":
-        surface, context = antiqua(surface, context, nib_size, field)
+    if font == "1":
+        surface, context = roman_square_capitals(surface, context, nib_size, field)
+    elif font == "2":
+        surface, context = antiqua_sans(surface, context, nib_size, field)
+    elif font == "3":
+        surface, context = blackletter(surface, context, nib_size, field)
+    else:
+        sys.exit()
 
     return surface, context
 
 
-def trojan(surface, context, step, field):
-    '''This function realizes the grid for the trojan font'''
+def roman_square_capitals(surface, context, step, field):
+    '''This function realizes the grid for the roman square capitals'''
     xpos = 0
     ypos = 0
+
     context.set_line_width(1)
     context.set_line_join(cairo.LINE_JOIN_MITER)
+
     while ypos < field[1]:
         while xpos < field[0]:
-            context.set_source_rgba(*aux_line)
             # Surrounding rectangles:
+            context.set_source_rgba(*aux_line)
                 # x, y, width, height
-            for args in [(step,      0,     10*step, step),
+            for args in [(step,    0,       10*step, step),
                          (step,    11*step, 10*step, step),
                          (0,       step,    step,    10*step),
                          (11*step, step,    step,    10*step)]:
                 context.rectangle(xpos + args[0], ypos + args[1], args[2], args[3])
                 context.stroke()
 
-            context.set_line_width(0.5)
             # Circles:
+            context.set_line_width(0.8)
                 # x, y
             for args in [(1*step,    2*step),
-                           (4*step,    2*step),
-                           (8*step,    2*step),
-                           (11*step,   2*step),
-                           (1*step,    10*step),
-                           (4*step,    10*step),
-                           (8*step,    10*step),
-                           (11*step,   10*step)]:
+                         (4*step,    2*step),
+                         (8*step,    2*step),
+                         (11*step,   2*step),
+                         (1*step,    10*step),
+                         (4*step,    10*step),
+                         (8*step,    10*step),
+                         (11*step,   10*step)]:
                 context.arc(xpos + args[0], ypos + args[1], step*0.96, 0, 2*math.pi)
                 context.stroke()
-            
-            context.set_line_width(1)
+
             # Small squares:
+            context.set_line_width(1)
             for y in range(0,10):
                 for x in range(0,10):
                     context.rectangle(xpos + (1 + x)*step, ypos + (1 + y)* step, step, step)
                     context.stroke()
 
-            context.set_source_rgba(*main_line)
             # Main squares:
+            context.set_source_rgba(*main_line)
             context.rectangle(xpos + step, ypos + step, 10*step, 10*step)
             context.stroke()
 
@@ -162,8 +184,8 @@ def trojan(surface, context, step, field):
     return surface, context
 
 
-def antiqua(surface, context, step, field):
-    '''This function realizes the grid for the antiqua font'''
+def antiqua_sans(surface, context, step, field):
+    '''This function realizes the grid for the antiqua sans'''
     xpos = 0
     ypos = 0
     # Define multiplier for this grid:
@@ -171,11 +193,11 @@ def antiqua(surface, context, step, field):
     # Find the vertical delta for a 25 degrees line for chosen paper:
     y_delta = field[0]*math.tan(math.radians(25))
 
-    context.set_source_rgba(*aux_line)
     context.set_line_width(1)
     context.set_line_join(cairo.LINE_JOIN_MITER)
 
     # Diagonal lines:
+    context.set_source_rgba(*aux_line)
     while ypos < field[1] + y_delta:
         context.move_to(0, ypos + multiplier*step)
         context.line_to(field[0], ypos - y_delta)
@@ -202,6 +224,42 @@ def antiqua(surface, context, step, field):
         context.line_to(field[0], ypos + 8*multiplier*step)
         context.stroke()
         ypos += 8*multiplier*step
+
+    return surface, context
+
+
+def blackletter(surface, context, step, field):
+    '''This function realizes the grid for the blackletter'''
+    xpos = 0
+    ypos = 0
+    # Define multiplier for this grid:
+    multiplier = math.cos(math.radians(30))
+
+    context.set_line_width(1)
+    context.set_line_join(cairo.LINE_JOIN_MITER)
+
+    # Aux vertical lines:
+    context.set_source_rgba(*aux_line)
+    while xpos < field[0]:
+        context.move_to(xpos, 0)
+        context.line_to(xpos, field[1])
+        xpos += multiplier*step
+
+    while ypos < field[1]:
+        # Aux horizontal lines:
+        context.set_source_rgba(*aux_line)
+        for arg in [0, 3, 4, 5, 6]:
+            context.move_to(0, ypos + arg*multiplier*step)
+            context.line_to(field[0], ypos + arg*multiplier*step)
+        context.stroke()
+        # Main horizontal lines:
+        context.set_source_rgba(*main_line)
+        context.move_to(0, ypos + 2*multiplier*step)
+        context.line_to(field[0], ypos + 2*multiplier*step)
+        context.move_to(0, ypos + 7*multiplier*step)
+        context.line_to(field[0], ypos + 7*multiplier*step)
+        context.stroke()
+        ypos += 9*multiplier*step
 
     return surface, context
 
